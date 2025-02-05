4cd10b854288d390d03b0c6e0cd3698691cb3f43
chickenlegyummy <163871645+chickenlegyummy@users.noreply.github.com>
2025-02-06 00:33:22 +0800

    feat: init again and again fk 4o mini

--- server/models/Quiz.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const QuizSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    video_url: { type: String },
-    teacher_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-});
-
-module.exports = mongoose.model('Quiz', QuizSchema);
--- server/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ['teacher', 'student'], default: 'student' }
-});
-
-module.exports = mongoose.model('User', UserSchema);
